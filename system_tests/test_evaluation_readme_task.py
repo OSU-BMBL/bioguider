cce463622739aa8e6055fdae1b4bc25d509846b9
fengsh <shaohong.feng@osumc.edu>
2025-11-19 08:40:56 -0500

    update test

--- system_tests/test_evaluation_readme_task.py
+++ system_tests/test_evaluation_readme_task.py
@@ -2,6 +2,7 @@ import pytest
 
 from bioguider.agents.evaluation_readme_task import EvaluationREADMETask
 
+@pytest.mark.skip()
 def test_EvaluationReadmeTask_RepoAgent(llm, step_callback, root_path):
     files = ["README.md"]
 
@@ -20,3 +21,18 @@ def test_EvaluationReadmeTask_RepoAgent(llm, step_callback, root_path):
         assert evaluation is not None
         assert evaluation.structured_evaluation.overall_score is not None
         assert evaluation.structured_evaluation.overall_score > 60 and evaluation.structured_evaluation.overall_score <= 100
+
+
+
+def test_EvaluationReadmeTask_deepmaps(llm, step_callback, root_path):
+    task = EvaluationREADMETask(
+        llm=llm,
+        repo_path=f"{root_path}/deepmaps",
+        gitignore_path=f"{root_path}/deepmaps/.gitignore",
+        step_callback=step_callback,
+    )
+    evaluations, files = task.evaluate()
+    assert evaluations is not None
+    assert files is not None and len(files) > 0
+    for file, evaluation in evaluations.items():
+        assert evaluation is not None
