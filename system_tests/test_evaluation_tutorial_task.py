97607a24472a4d2fc40c3cf1c972caba699e50d4
fengsh <shaohong.feng.78@gmail.com>
2026-02-11 11:53:39 -0500

    minor changes

--- system_tests/test_evaluation_tutorial_task.py
+++ system_tests/test_evaluation_tutorial_task.py
@@ -292,6 +292,7 @@ def test_EvaluationTutorialTask_evaluate_on_seurat(llm, step_callback, root_path
     assert evaluations is not None
     assert files is not None
 
+@pytest.mark.skip()
 def test_EvaluationTutorialTask_evaluate_on_BioGSP(llm, step_callback, root_path, data_folder):
     code_structure_db = CodeStructureDb(
         author="BMEngineeR",
@@ -331,3 +332,101 @@ def test_EvaluationTutorialTask_evaluate_on_BioGSP(llm, step_callback, root_path
 
     assert evaluations is not None
     assert files is not None
+
+
+@pytest.mark.skip()
+def test_EvaluationTutorialTask_only_evaluate_on_scanpy(llm, step_callback, root_path, data_folder):
+    code_structure_db = CodeStructureDb(
+        author="scverse",
+        repo_name="scanpy",
+        data_folder=data_folder
+    )
+    summarized_files_db = SummarizedFilesDb(
+        author="scverse",
+        repo_name="scanpy",
+        data_folder=data_folder,
+    )
+    code_structure_builder = CodeStructureBuilder(
+        repo_path=f"{root_path}/scanpy",
+        gitignore_path=f"{root_path}/scanpy/.gitignore",
+        code_structure_db=code_structure_db,
+    )
+    code_structure_builder.build_code_structure()
+    task = EvaluationTutorialTask(
+        llm=llm,
+        repo_path=f"{root_path}/scanpy",
+        gitignore_path=f"{root_path}/scanpy/.gitignore",
+        step_callback=step_callback,
+        code_structure_db=code_structure_db,
+        summarized_files_db=summarized_files_db,
+    )
+    files = [
+        "notebooks/dask.ipynb", 
+        # "notebooks/basic-scrna-tutorial.ipynb", 
+        # "notebooks/tutorial_pearson_residuals.ipynb", 
+        # "notebooks/paga-paul15.ipynb", 
+        # "notebooks/pbmc3k.ipynb", 
+        # "notebooks/integrating-data-using-ingest.ipynb", 
+        # "notebooks/how-to/plotting-with-marsilea.ipynb", 
+        # "notebooks/how-to/cell-cycle.ipynb", 
+        # "notebooks/how-to/knn-transformers.ipynb", 
+        # "notebooks/scanpy_workshop/day2_02_GeneSetEnrichmentAnalysis.ipynb", 
+        # "notebooks/scanpy_workshop/day1_01_GColab.ipynb", 
+        # "notebooks/scanpy_workshop/day1_01_GColabs_solutions.ipynb", 
+        # "notebooks/scanpy_workshop/day2_03_RNAvelocity.ipynb", 
+        # "notebooks/scanpy_workshop/day2_02_GSEA_Colabs.ipynb", 
+        # "notebooks/scanpy_workshop/day1_01.ipynb", 
+        # "notebooks/scanpy_workshop/day1_01_solutions.ipynb", 
+        # "notebooks/scanpy_workshop/day2_01_DifferentialExpression.ipynb", 
+        # "notebooks/scanpy_workshop/day2_01_DE_Colabs.ipynb", 
+        # "docs/tutorials/plotting/advanced.ipynb", 
+        # "docs/tutorials/plotting/core.ipynb", 
+        # "docs/tutorials/experimental/pearson_residuals.ipynb", 
+        # "docs/tutorials/experimental/dask.ipynb", 
+        # "docs/tutorials/basics/clustering-2017.ipynb", 
+        # "docs/tutorials/basics/clustering.ipynb", 
+        # "docs/tutorials/basics/integrating-data-using-ingest.ipynb", 
+        # "docs/tutorials/trajectories/paga-paul15.ipynb"
+    ]
+    evaluations, files = task._evaluate(files)
+    assert evaluations is not None
+    assert files is not None
+
+def test_EvaluationTutorialTask_on_emitgcl_files(llm, step_callback, root_path, data_folder):
+    code_structure_db = CodeStructureDb(
+        author="mtduan",
+        repo_name="emitgcl",
+        data_folder=data_folder
+    )
+    summarized_files_db = SummarizedFilesDb(
+        author="mtduan",
+        repo_name="emitgcl",
+        data_folder=data_folder,
+    )
+    code_structure_builder = CodeStructureBuilder(
+        repo_path=f"{root_path}/emitgcl",
+        gitignore_path=f"{root_path}/emitgcl/.gitignore",
+        code_structure_db=code_structure_db,
+    )
+    code_structure_builder.build_code_structure()
+    task = EvaluationTutorialTask(
+        llm=llm,
+        repo_path=f"{root_path}/emitgcl",
+        gitignore_path=f"{root_path}/emitgcl/.gitignore",
+        step_callback=step_callback,
+        code_structure_db=code_structure_db,
+        summarized_files_db=summarized_files_db,
+    )
+    files = [
+        "EmitGCL/README.md",
+        "EmitGCL/Tutorial/EmitGCL-example.ipynb",
+    ]
+    
+    files = task._sanitize_files(files)
+    evaluations, files = task._evaluate(files)
+    assert evaluations is not None
+    assert files is not None
+
+
+
+
